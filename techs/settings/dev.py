d1491667293626624341059f6507ee555bda7088
Eduardo Vela <eduardo3vela@gmail.com>
2022-10-07 16:12:45 +0200

    Add first draft for Wagtail site

---
+++ techs/settings/dev.py
@@ -0,0 +1,36 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-@5sbr1ra0=hmvcf1)k&5#lr!s$c4a-20(auhiqdd2d25eq2@_2'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*']
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+INSTALLED_APPS = INSTALLED_APPS + [
+    'debug_toolbar',
+]
+
+MIDDLEWARE = MIDDLEWARE + [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+]
+
+INTERNAL_IPS = ("127.0.0.1", "172.17.0.1")
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+
+try:
+    from .local import *
+except ImportError:
+    pass
