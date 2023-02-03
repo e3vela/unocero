e4502106f01d473e9ac3cafda5e4dc0a2ac9cb66
Eduardo Vela <eduardo3vela@gmail.com>
2023-02-03 16:31:59 +0100

    Retrieve production variables from env variables

--- techs/settings/production.py
+++ techs/settings/production.py
@@ -1,3 +1,8 @@
+import os
+
+import dj_database_url
+from django.core.management.utils import get_random_secret_key
+
 from .base import *
 
 DEBUG = False
@@ -5,4 +10,12 @@ DEBUG = False
 try:
     from .local import *
 except ImportError:
-    pass
+    SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
+
+    ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
+
+    DEBUG = os.getenv("DEBUG", "False") == "True"
+
+    DATABASES = {
+        "default": dj_database_url.parse(os.environ.get("DATABASE_URL")),
+    }
