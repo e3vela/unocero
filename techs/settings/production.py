b3b63471f34b34cdbb837565e492e83f0204525f
Eduardo Vela <eduardo3vela@gmail.com>
2023-02-04 18:58:28 +0100

    Use local.py instead for server settings

--- techs/settings/production.py
+++ techs/settings/production.py
@@ -1,8 +1,3 @@
-import os
-
-import dj_database_url
-from django.core.management.utils import get_random_secret_key
-
 from .base import *
 
 DEBUG = False
@@ -10,14 +5,4 @@ DEBUG = False
 try:
     from .local import *
 except ImportError:
-    SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
-
-    ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
-
-    DEBUG = os.getenv("DEBUG", "False") == "True"
-
-    DATABASE_URL = os.getenv("DATABASE_URL")
-
-    DATABASES = {
-        "default": dj_database_url.config(),
-    }
+    pass
